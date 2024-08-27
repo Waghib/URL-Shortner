30a80b0402b1a36c771ada16756cba34b1463d3f
Waghib <Waghibahmad30@gmail.com>
2024-08-27 23:00:27 +0500

    index updated

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require('express');
+const {connectToMongoDB} = require('./connect');
+const urlRoute = require('./routes/url');
+
 const app = express();
 const port = 3000;
 
+connectToMongoDB('mongodb://localhost:27017/short-url')
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((err) => {
+        console.error(err);
+    });
 
+    
+
+app.use('/url', urlRoute);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
