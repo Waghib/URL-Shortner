9edf239544c327285539d7c922a35ada05cda29c
Waghib <Waghibahmad30@gmail.com>
2024-09-01 01:35:30 +0500

    created a middleware and assigned it to the user route

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const path = require('path');
+const cookieParser = require('cookie-parser');
 const {connectToMongoDB} = require('./connect');
+
+const {restrictToLoggedinUserOnly} = require('./middlewares/auth');
 
 const URL = require('./models/url');
 
@@ -25,13 +28,14 @@ app.set('views', path.resolve('./views'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(express.static('public'));
+app.use(cookieParser());
 
 // app.get("/test", async (req, res) => {
 //     const allUrls = await URL.find({});
 //     return res.render('home', {urls: allUrls});
 // });
 
-app.use('/url', urlRoute);
+app.use('/url',restrictToLoggedinUserOnly, urlRoute);
 app.use('/', staticRoute);
 app.use('/user', userRoute);
 
