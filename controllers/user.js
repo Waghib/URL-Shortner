068b6cca5f13ef7635a9f5eb5071dc7c87c54f98
Waghib <Waghibahmad30@gmail.com>
2024-09-01 01:05:00 +0500

    created a user login view and route

--- controllers/user.js
+++ controllers/user.js
@@ -4,9 +4,20 @@ async function handleUserSignup(req, res) {
   const { name, email, password } = req.body;
   await User.create({ name, email, password });
 
-  return res.render("home");
+  return res.redirect("/");
+}
+
+async function handleUserLogin(req, res) {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email, password });
+  if (!user) {
+    return res.render("login", { error: "Invalid email or password" });
+  }
+
+  return res.redirect("/");
 }
 
 module.exports = {
-    handleUserSignup,
+  handleUserSignup,
+  handleUserLogin,
 };
