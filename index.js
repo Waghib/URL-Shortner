13045933c6659a41b29f9fc74bb465fab2093e3f
Waghib <Waghibahmad30@gmail.com>
2024-09-08 22:04:42 +0500

    changes in the index.js

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const {connectToMongoDB} = require('./connect');
 
+// middlewares
 const {restrictToLoggedinUserOnly, checkAuth} = require('./middlewares/auth');
 
+// models
 const URL = require('./models/url');
 
+// routes
 const urlRoute = require('./routes/url');
 const staticRoute = require('./routes/staticRouter');
 const userRoute = require('./routes/user');
@@ -14,6 +17,7 @@ const userRoute = require('./routes/user');
 const app = express();
 const port = 3000;
 
+// Connect to MongoDB
 connectToMongoDB('mongodb://localhost:27017/short-url')
     .then(() => {
         console.log('Connected to MongoDB');
@@ -22,9 +26,11 @@ connectToMongoDB('mongodb://localhost:27017/short-url')
         console.error(err);
     });
 
+// Set view engine
 app.set('view engine', 'ejs');
 app.set('views', path.resolve('./views'));
 
+// Middlewares
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(express.static('public'));
@@ -35,6 +41,7 @@ app.use('/url',restrictToLoggedinUserOnly, urlRoute);
 app.use('/user', userRoute);
 app.use('/', checkAuth, staticRoute);
 
+// Redirect to the original URL
 app.get('/url/:shortId', async (req, res) => {
     const shortid = req.params.shortId;
     const entry = await URL.findOneAndUpdate({
